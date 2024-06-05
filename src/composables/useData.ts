be316b5ba1db88ececdedfaac9d8d9cb4f1bab39
hg_it_nuc <2675263825@qq.com>
2024-06-05 21:38:37 +0800

    commit

--- src/composables/useData.ts
+++ src/composables/useData.ts
@@ -18,11 +18,13 @@ export const useData = () => {
    * 网站列表
    */
   const websites = ref<Website[]>([
-    { name: 'homepage', href: 'https://onedayxyy.cn/', icon: 'mdi:web-check' },
+    { name: '个人首页', href: 'https://onedayxyy.cn/', icon: 'mdi:web-check' },
+    { name: '知识库', href: 'https://onedayxyy.cn/docusaurus/', icon: 'https://files.dshuais.com/images/logo/k.png', iconType: 'img' },
     { name: '博客', href: 'http://47.100.215.163:8090/', icon: 'fa6-solid:blog' },
     // { name: 'create-kd', href: 'https://create-keep-design.dshuais.com/', icon: 'k' },
-    { name: '知识库', href: 'https://onedayxyy.cn/docusaurus/', icon: 'https://files.dshuais.com/images/logo/k.png', iconType: 'img' },
     { name: '导航', href: 'https://onedayxyy.cn/docusaurus/docs/tool/', icon: 'https://files.dshuais.com/images/logo/dm.png', iconType: 'img' },
+    { name: '相册', href: 'http://47.100.215.163:8080/', icon: 'mdi:web-check' },
+    { name: '音乐', href: 'https://onedayxyy.cn/musics/', icon: 'https://files.dshuais.com/images/logo/k.png', iconType: 'img' },
   ])
 
   return {
