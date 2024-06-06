73debd41267ff718f855041a730bff7b262b9723
hg_it_nuc <2675263825@qq.com>
2024-06-06 08:10:06 +0800

    data commit

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -13,11 +13,11 @@ export default defineComponent({
   setup() {
     return () => (
       <div class="absolute lg:fixed bottom-0 lg:w-full py-2 bg-[#ffffff30] flex justify-center items-center flex-col lg:flex-row text-gray-300 w-[calc(100%+2rem)] ml-[-1rem] lg:ml-0">
-        <div>Copyright&nbsp;&copy; 2022 - {new Date().getFullYear()} <NuxtLink href="https://dshuais.com/" class="hover:text-white">DuShuai</NuxtLink>.</div>
+        <div>Copyright&nbsp;&copy; 2023 - {new Date().getFullYear()} <NuxtLink href="https://dshuais.com/" class="hover:text-white">DuShuai</NuxtLink>.</div>
         <div class="hidden lg:block">&nbsp;&nbsp;&&nbsp;&nbsp;</div>
-        <div>Powered by <NuxtLink href="https://github.com/dshuais" target="_blank" class="hover:text-white">dshuais</NuxtLink>.</div>
+        <div>Powered by <NuxtLink href="https://github.com/dshuais" target="_blank" class="hover:text-white">One</NuxtLink>.</div>
         <div class="hidden lg:block">&nbsp;&nbsp;&&nbsp;&nbsp;</div>
-        <NuxtLink href="https://beian.miit.gov.cn" target="_blank" class="hover:text-white">鄂ICP备2022011303号-1</NuxtLink>
+        <NuxtLink href="https://beian.miit.gov.cn" target="_blank" class="hover:text-white">陇ICP备2023002645号</NuxtLink>
       </div>
     )
   }
